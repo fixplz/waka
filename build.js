15b4c6300f291c5c10f33d9144e5d6729ad2be5e
Gustavs T <fixplzsecrets@gmail.com>
2014-07-04 21:31:21 +0300

    Organize build script

--- build.js
+++ build.js
@@ -10,31 +10,57 @@ function write(f, contents) {
   fs.writeFileSync(f, contents, 'utf8')
 }
 
+function writeInspect(f, ast) {
+  write(f, util.inspect(ast, { depth: null }))
+}
 
 
-var parser = require('./minipeg-old.js')
 
-var reader = parser(read('reader.peg'))
-write('./temp/reader.js', reader)
-var reader_func = eval(reader)
+var reader1 = generateNewReaderOldSyntax()
 
-console.log("\n== Running\n")
+console.log("\n== Running old reader\n")
 
-var reader2_ast = reader_func(read('reader2.peg')) 
-write('./temp/reader2-gen1-ast.txt', util.inspect(reader2_ast, { depth: null }))
+var reader2_ast = runOldReader(reader1)
 
-console.log("== Running new parser\n")
+console.log("== Running new generator\n")
 
-vm.runInThisContext(read('Minipeg2.js'), 'Minipeg2.js')
+var reader2 = generateNewReaderNewGenerator(reader2_ast)
 
-var reader2_gen2 = buildParser(reader2_ast, { debug: true })
-write('./temp/reader2-gen2.js', reader2_gen2.toString())
-var reader2_func = reader2_gen2()
-
-reader2_func.setDoc(read('test.peg'))
-var reader2_out = reader2_func.parse()
-
-console.log('Success:', reader2_out.success, 'Done:', reader2_out.done)
-write('./temp/reader2-gen2-ast.txt', util.inspect(reader2_out.result, { depth: null }))
+runNewReader(reader2)
 
 console.log('\n== Done!\n')
+
+
+function generateNewReaderOldSyntax() {
+  var p = require('./minipeg-old.js')
+
+  var readerSrc = p(read('reader.peg'))
+  write('./temp/reader.js', readerSrc)
+
+  return eval(readerSrc)
+}
+
+function runOldReader(reader) {
+  var ast = reader(read('reader2.peg'))
+  writeInspect('./temp/reader2-gen1-ast.txt', ast)
+  return ast
+}
+
+function generateNewReaderNewGenerator(ast) {
+  vm.runInThisContext(read('Minipeg2.js'), 'Minipeg2.js')
+
+  var reader2Fn = buildParser(ast, { debug: true })
+  write('./temp/reader2-gen2.js', reader2Fn.toString())
+
+  return reader2Fn()
+}
+
+function runNewReader(reader) {
+  reader.setDoc(read('test.peg'))
+  var result = reader.parse()
+
+  console.log('Success:', result.success, 'Done:', result.done)
+  write('./temp/reader2-gen2-ast.txt', util.inspect(result.result, { depth: null }))
+
+  return result
+}
