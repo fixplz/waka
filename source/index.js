e7adda74771f5d12f7badae51c9fa29d1ba64154
Gustavs T <fixplzsecrets@gmail.com>
2015-02-08 18:35:05 +0200

    Add getSourceStandalone

--- source/index.js
+++ source/index.js
@@ -1,63 +1,84 @@
-var buildParser = require('./compiler').buildParser
+var compiler = require('./compiler')
 
-var reader = Function(buildParser(require('./parser.peg.json')))()
+var pegParser = Function(compiler.buildParser(require('./parser.peg.json')))()
 
 module.exports = Waka
 
 function Waka(peg, opts) {
   var parser = Function(Waka.getSource(peg, opts))()
   return _waka(parser, (opts && opts.startRule) || 'Start')
-
-  function _waka(parser, startRule) {
-    if(! startRule)
-      throw new Error('no start rule given')
-
-    if(! parser.rules[startRule])
-      throw new Error('start rule missing: ' + JSON.stringify(startRule))
-
-    return {
-      getState: function() {
-        return parser.state
-      },
-
-      getTrace: function(message) {
-        return (message ? message + '\n' : '') + parser.state.traceLine()
-      },
-
-      exec: function(input) {
-        parser.state.setInput(input)
-
-        try {
-          var value = parser.rules[startRule]()
-        }
-        catch(err) {
-          var error = err
-        }
-
-        if(error == null) {
-          if(! parser.state.isEOF())
-            var error = new Error('input not fully parsed')
-        }
-
-        return {
-          success: error == null,
-          value: value,
-          error: error
-        }
-      },
-
-      startWith: function(rule) {
-        return _waka(parser, rule)
-      },
-    }
-  }
 }
 
+Waka.compiler = compiler
+
 Waka.getAST = function(peg) {
-  reader.state.setInput(peg)
-  return reader.rules.Start()
+  var result = _waka(pegParser, 'Start').exec(peg)
+
+  if(result.error) {
+    console.error('Error in PEG: ', result.error.message)
+    console.error(pegParser.state.traceLine())
+    throw result.error
+  }
+
+  return result.value
 }
 
 Waka.getSource = function(peg, opts) {
-  return buildParser(Waka.getAST(peg), opts)
+  return compiler.buildParser(Waka.getAST(peg), opts)
+}
+
+Waka.getSourceStandalone = function(peg, opts) {
+  function iif(str) {
+    return '(function(){' + str + '})()'
+  }
+  return iif(
+    _waka.toString() + ';\n' +
+      'return _waka(' +
+        iif(compiler.buildParser(Waka.getAST(peg), opts))
+        + ',' + JSON.stringify(opts && 'startRule' in opts ? opts.startRule : 'Start') + ')'
+  )
+}
+
+function _waka(parser, startRule) {
+  if(startRule && ! parser.rules[startRule])
+    throw new Error('start rule missing: ' + JSON.stringify(startRule))
+
+  return {
+    getState: function() {
+      return parser.state
+    },
+
+    getTrace: function(message) {
+      return (message ? message + '\n' : '') + parser.state.traceLine()
+    },
+
+    exec: function(input) {
+      if(! startRule)
+        throw new Error('no start rule given')
+
+      parser.state.setInput(input)
+
+      try {
+        var value = parser.rules[startRule]()
+      }
+      catch(err) {
+        var error = err
+      }
+
+      if(error == null) {
+        if(! parser.state.isEOF())
+          var error = new Error('input not fully parsed')
+      }
+
+      return {
+        success: error == null,
+        value: value,
+        error: error
+      }
+    },
+
+    startWith: function(rule) {
+      return _waka(parser, rule)
+    },
+  }
 }
